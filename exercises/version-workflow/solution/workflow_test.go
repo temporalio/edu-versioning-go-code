6598d0b3da904aa633a961b497475fbfda864ca9
Tom Wheeler <tom@temporal.io>
2023-11-17 10:33:40 -0600

    Ran the Go formatter on the code, eliminating trailing whitespace on one line

--- exercises/version-workflow/solution/workflow_test.go
+++ exercises/version-workflow/solution/workflow_test.go
@@ -14,7 +14,7 @@ func TestReplayWorkflowHistoryFromFile(t *testing.T) {
 
 	// NOTE: Your path will be that of the file you downloaded, such as
 	// /Users/twheeler/Downloads/02c502fe-846c-4493-abfd-b6909935693c_events.json
-	// instead of the one you see here (which was changed so that you can 
+	// instead of the one you see here (which was changed so that you can
 	// run the test).
 	err := replayer.ReplayWorkflowHistoryFromJSONFile(nil, "history_for_original_execution.json")
 
